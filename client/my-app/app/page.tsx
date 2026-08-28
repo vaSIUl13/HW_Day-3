386219dea05153e1c81bfe3ad623c91c66703c96
default <default@gmail.com>
2026-08-28 15:27:30 +0300

    feat: Add styles fot clicker

--- client/my-app/app/page.tsx
+++ client/my-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const[counter, setCounter] = useState(0);
   
   return <>
-  <div>
+  <div className="flex flex-col items-center justify-center h-screen">
     <h1>Counter: {counter}</h1>
     <button onClick={() => setCounter(counter + 1)}>Increment</button>
     <button onClick={() => setCounter(counter - 1)}>Decrement</button>
