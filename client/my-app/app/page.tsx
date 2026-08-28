60ab0e156f171bebecec6e60fbf40f53ff925c4b
vaSIUl13 <mygchinkavasil@gmail.com>
2026-08-28 15:46:01 +0300

    feat: Add header with title to Home page (#1)
    Co-authored-by: default <default@gmail.com>

--- client/my-app/app/page.tsx
+++ client/my-app/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   const[counter, setCounter] = useState(0);
   
   return <>
+  <header>
+    <h1>Homework day 3</h1>
+  </header>
   <div className="flex flex-col items-center justify-center h-screen">
     <h1>Counter: {counter}</h1>
     <button onClick={() => setCounter(counter + 1)}>Increment</button>
