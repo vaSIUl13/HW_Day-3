10c0e8b0420db7c702d0315207283be30c2daa2b
default <default@gmail.com>
2026-08-28 15:27:00 +0300

    feat: Add counter to the page

--- client/my-app/app/page.tsx
+++ client/my-app/app/page.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 
 export default function Home() {
+  const[counter, setCounter] = useState(0);
+  
   return <>
-    <h1>Hello World</h1>
+  <div>
+    <h1>Counter: {counter}</h1>
+    <button onClick={() => setCounter(counter + 1)}>Increment</button>
+    <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+  </div>
   </>
 }
